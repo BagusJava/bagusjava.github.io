7e2c5a6a695a5dc50c6e5043faaf56877ac636a3
Bagus Java <bagusjava7@gmail.com>
2021-03-07 15:17:12 +0700

    change twitter

--- src/portfolio.js
+++ src/portfolio.js
@@ -368,7 +368,7 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "bagusjava", //Replace "twitter" with your twitter username without @
+  userName: "bagusjavaid", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
